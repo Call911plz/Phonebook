44aa9fc50da91e23a50228da46959886d3b5c745
Hieu Truong <htruong6219@gmail.com>
2025-04-10 14:39:38 -0700

    feat: add CategoryDatabaseManager
    need to test functions

--- Model/CategoryDatabaseManager.cs
+++ Model/CategoryDatabaseManager.cs
@@ -1,28 +1,43 @@
 
-// class CategoryDatabaseManager : DatabaseManagerBase
-// {
-//     public override void CreateEntity()
-//     {
-//         base.CreateEntity();
-//     }
-    
-//     public new void GetAllEntity() // TODO: update this
-//     {
-//         using var db = new DatabaseContext();
+using Microsoft.EntityFrameworkCore;
 
-//         foreach(Category category in db.Categories)
-//         {
-//             Console.WriteLine(category);
-//         }
-//     }
+class CategoryDatabaseManager : DatabaseManagerBase
+{
+    public async Task CreateEntityAsync(Category category)
+    {
+        using var db = new DatabaseContext();
+        db.Add(category);
 
-//     public override void UpdateEntity()
-//     {
-//         base.UpdateEntity();
-//     }
+        await db.SaveChangesAsync();
+    }
 
-//     public override void DeleteEntity()
-//     {
-//         base.DeleteEntity();
-//     }
-// }
+    public new List<Category> GetAllEntity()
+    {
+        using var db = new DatabaseContext();
+        return db.Categories.ToList();   
+    }
+
+    public async Task UpdateEntityAsync(Category updatedCategory)
+    {
+        using var db = new DatabaseContext();
+        var categoryFromDB = await db.Categories
+            .Where(category => category.Id == updatedCategory.Id)
+            .FirstAsync();
+        
+        categoryFromDB.Name = updatedCategory.Name;
+
+        await db.SaveChangesAsync();
+    }
+
+    public async Task DeleteEntityAsync(Category categoryToDelete)
+    {
+        using var db = new DatabaseContext();
+        var categoryFromDB = await db.Categories
+            .Where(category => category.Id == categoryToDelete.Id)
+            .FirstAsync();
+
+        db.Remove(categoryFromDB);
+
+        await db.SaveChangesAsync();
+    }
+}
